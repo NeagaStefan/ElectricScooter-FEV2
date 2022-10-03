6c9956704dd26e8a0701ed4de324355e0c46d8c1
NeagaStefan <neagastfean@gmail.com>
2022-10-03 16:35:04 +0300

    v1.3

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import AuthService from "../services/auth.service";
-import {useNavigate} from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import customerService from "../services/CustomerService";
 import DropDownComponent from "../components/DropDownComponent";
@@ -75,119 +75,121 @@ function Explore() {
         },
     ]
 
-    const navigate = useNavigate()
+
     const [userName, setUserName] = useState('')
     const [scootersAvailable, setScootersAvailable] = useState([])
     const [selected, setSelected] = useState({})
-    const [selectedPos, setSelectedPos] = useState({})
+    const [selectedPos, setSelectedPos] = useState(options[0])
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
 
-
-    const onLogout =()=> {
-        AuthService.logout()
-        navigate('/sign-in')
+    if (!AuthService.getCurrentUser()){
+        toast.error("Please log in first")
+        return <Navigate to={'/sign-in'}/>
     }
-    useEffect(()=>{
 
-    async function setUserNameF(){
-        return  await AuthService.getCurrentUser().username
-    }
-    setUserNameF().then(r=>setUserName(r))
+    useEffect(() => {
 
-    async function setScooterAvailable(){
+        async function setUserNameF() {
+            return await AuthService.getCurrentUser().username
+        }
 
-       const data = await ScooterService.getAvailableScooters()
-        setSelected(data.data[0])
-        return data
-    }
-    setScooterAvailable().then(r=>setScootersAvailable(r.data))
+        setUserNameF().then(r => setUserName(r))
+
+        async function setScooterAvailable() {
+
+            const data = await ScooterService.getAvailableScooters()
+            setSelected(data.data[0])
+            return data
+        }
+
+        setScooterAvailable().then(r => setScootersAvailable(r.data))
         setSelected(scootersAvailable[0])
-    },[])
+    }, [])
 
     const onStart = () => {
         try {
             customerService.startRenting(userName, selected.scooterId).then(
                 toast.success("The renting has started"))
-        }catch (e){
-            toast.error(e.message)
+        } catch (error) {
+            toast.error(error.message)
         }
-        // setStarted(true)
-        //todo when start is pressed dropdown is changed with a text ?
-        console.log(selected.scooterId,userName)
 
     }
 
     const onStop = () => {
-        if(selectedPos!=null) {
-            try{
-            customerService.stopRenting(userName, selected.scooterId, selectedPos.value).then(
-                toast.success("The rental has stopped")
-            )
-            }catch (e){
+        console.log(selectedPos)
+        if (selectedPos != null) {
+            try {
+                customerService.stopRenting(userName, selected.scooterId, selectedPos.value).then(
+                    toast.success("The rental has stopped")
+                )
+            } catch (error) {
                 toast.error("Aloooo")
             }
-        }else {
+        } else {
             toast.error("Please select a zone first")
         }
-        // setStarted(false)
     }
 
-        const [time, setTime] = useState(0);
-        const [running, setRunning] = useState(false);
 
-        useEffect(() => {
-            let interval;
-            if (running) {
-                interval = setInterval(() => {
-                    setTime((prevTime) => prevTime + 10);
-                    console.log(time)
-                }, 10);
-            } else if (!running) {
-                clearInterval(interval);
-            }
-            return () => clearInterval(interval);
-        }, [running]);
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+                console.log(time)
+            }, 10);
+        } else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [running]);
 
 
-        const renderedClock = (
-            <div className="pageContainer">
-                <div className="numbers">
-                    <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-                    <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
-                </div>
-                <div className="buttons">
-                    {!running &&
-                    <button className={"formButtonActive"} onClick={() => {setRunning(true)
-                        console.log(running)
-                    onStart()
-                    }}>Start renting</button>
-                    }
-                    {running &&
-                        <button  type="button" class="btn btn-danger" onClick={() => {
-                            setRunning(false)
-                            onStop(time)
-                            setTime(0)
-                        }}>Stop renting</button>
-                    }
-                </div>
+    const renderedClock = (
+        <div className="pageContainer">
+            <div className="numbers">
+                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
             </div>
-        );
+            <div className="buttons">
+                {!running &&
+                    <button className={"formButtonActive"} onClick={() => {
+                        setRunning(true)
+                        onStart()
+                    }}>Start renting</button>
+                }
+                {running && selectedPos &&
 
+                    <button type="button" className="btn btn-danger" onClick={() => {
+                        setRunning(false)
+                        onStop()
+                        setTime(0)
+                    }}>Stop renting</button>
+                }
+            </div>
+        </div>
+    );
 
 
     return (
+        <div>
+            <h1>Electronic scooter app</h1>
+            <div className={"body"}>
+                <div className={"form-signin"}>
+                    {!running &&
+                        <DropDownComponent label="Select one of the available scooters" options={scootersAvailable}
+                                           selected={selected} onSelectedChange={setSelected}/>
+                    }
+                    {running &&
+                        <DropDownLocations label="Select one safe zones" options={options} selectedPos={selectedPos}
+                                           onSelectedChange={setSelectedPos}/>
+                    }
+                    {renderedClock}
 
-        <div className={"pageContainer"}>
-            <h1>Explore Page</h1>
-            {!running &&
-                <DropDownComponent label="Select one of the available scooters" options={scootersAvailable}
-                                   selected={selected} onSelectedChange={setSelected}/>
-            }
-            {running &&
-                <DropDownLocations label="Select one safe zones" options={options} selectedPos={selectedPos}
-                                   onSelectedChange={setSelectedPos}/>
-            }
-            {renderedClock}
-
+                </div>
+            </div>
         </div>
     )
 }
